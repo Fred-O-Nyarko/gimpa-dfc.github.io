c4f4821f0b2567aff861ddb7af383f58126314cb
Frederick Obeng-Nyarko <nernuer@gmail.com>
2025-09-04 11:03:51 +0000

    Update academic year in footer of Timetable component

--- app/components/Timetable.tsx
+++ app/components/Timetable.tsx
@@ -11,14 +11,14 @@ export const Timetable: React.FC<TimetableProps> = ({ data }) => {
   return (
     <div className="timetable-container">
       <Header university={data.university} />
-      
+
       <main className="timetable-main">
         <DaySection day="friday" timeSlots={data.schedule.friday} />
         <DaySection day="saturday" timeSlots={data.schedule.saturday} />
       </main>
 
       <footer className="timetable-footer">
-        <p>Generated for Academic Year 2024/2025</p>
+        <p>Generated for Academic Year 2025/2026</p>
       </footer>
     </div>
   );
